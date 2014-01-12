08ba3d317ea8f888aef2bde47ed78bd3073332f1
Steve Buzonas <steve@fancyguy.com>
2014-01-12 16:14:31 -0500

    added ido addon

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(addon-ido t)
  '(addon-smooth-scrolling t)
  '(addon-undo-tree t)
  '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
