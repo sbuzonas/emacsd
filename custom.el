1e2f6c4fe5078033f08226f8110a5b9fbd0b4691
Steve Buzonas <steve@fancyguy.com>
2014-01-12 17:39:39 -0500

    added grep add-on

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(addon-dired t)
+ '(addon-grep t)
  '(addon-guide-key t)
  '(addon-ido t)
  '(addon-magit t)
