7ec0f52df349c744cabb002bf196b0d793ca8590
Steve Buzonas <steve@fancyguy.com>
2014-01-12 16:41:47 -0500

    added dired add-on

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(addon-dired t)
  '(addon-guide-key t)
  '(addon-ido t)
  '(addon-org t)
