5c51c6f4a5b52e20ee45ea2e29dda5eedee297f2
Steve Buzonas <sbuzonas@carnegielearning.com>
2014-01-27 14:21:06 -0500

    add man configuration to use as primary pager

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Man-notify-method (quote bully))
  '(addon-circe t)
  '(addon-dired t)
  '(addon-grep t)
