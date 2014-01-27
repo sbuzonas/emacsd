79ad219d16e4f41ff938f3cca49336df5ad7b210
Steve Buzonas <sbuzonas@carnegielearning.com>
2014-01-27 14:16:40 -0500

    add support for day/night theme changes

--- appearance.el
+++ appearance.el
@@ -3,6 +3,12 @@
       color-theme-is-global t
       truncate-partial-width-windows nil)
 
+(defvar night-hour 18
+  "When to start with night theme.")
+
+(defvar day-hour 8
+  "When to start with day theme.")
+
 ;; Highlight current line
 (global-hl-line-mode 1)
 
@@ -14,31 +20,40 @@
   (when (file-directory-p path)
     (add-tol-list 'custom-theme-load-path path)))
 
+
+
 ;; Initialize dark theme
 (require-package 'tango-2-theme)
 (setq dark-theme 'tango-2)
 
 ;; Initialize light theme
 (require-package 'solarized-theme)
-(setq light-theme 'solarized-light)
+(require-package 'zenburn-theme)
+(setq light-theme 'zenburn)
+
+(setq current-theme nil)
 
 ;; Dark theme
 (defun use-dark-theme ()
   (interactive)
-  (disable-theme light-theme)
-  (load-theme dark-theme)
-  (setq current-theme dark-theme)
-  (when (boundp 'slbmeh/dark-font)
-    (set-face-attribute 'default nil :font slbmeh/dark-font)))
+  (if (not (string= current-theme dark-theme))
+      (progn
+	(disable-theme light-theme)
+	(load-theme dark-theme)
+	(setq current-theme dark-theme)
+	(when (boundp 'slbmeh/dark-font)
+	  (set-face-attribute 'default nil :font slbmeh/dark-font)))))
 
 ;; Light theme
 (defun use-light-theme ()
   (interactive)
-  (disable-theme dark-theme)
-  (load-theme light-theme)
-  (setq current-theme light-theme)
-  (when (boundp 'slbmeh/light-font)
-    (set-face-attribute 'default nil :font slbmeh/light-font)))
+  (if (not (string= current-theme light-theme))
+      (progn
+	(disable-theme dark-theme)
+	(load-theme light-theme)
+	(setq current-theme light-theme)
+	(when (boundp 'slbmeh/light-font)
+	  (set-face-attribute 'default nil :font slbmeh/light-font)))))
 
 ;; Toggle themes
 (defun swap-theme-contrast ()
@@ -49,7 +64,18 @@
 
 (global-set-key (kbd "<f9>") 'swap-theme-contrast)
 
-(use-dark-theme)
+(defun color-theme-timer ()
+  "Sets color theme according to current time. Customize `night-hour' and `day-hour'."
+  (interactive)
+  (let ((hour (nth 2 (decode-time)))
+	(minute (nth 1 (decode-time))))
+    (if (or (>= hour night-hour) (< hour day-hour))
+	(use-light-theme) ;; Night time color
+      (use-dark-theme))))
+
+(color-theme-timer)
+
+(add-hook 'auto-save-hook 'color-theme-timer)
 
 ;; Don't defer screen updates when performing operations
 (setq redisplay-dont-pause t)
