8aca0856224bcb44e41466f28c3d179dddb19f38
Steve Buzonas <sbuzonas@carnegielearning.com>
2014-02-10 12:46:37 -0500

    increase undo maximum size

--- add-ons/undo-tree.el
+++ add-ons/undo-tree.el
@@ -11,6 +11,7 @@
 
 (after 'undo-tree
   (global-undo-tree-mode)
+  (setq undo-tree-outer-limit 36000000)
   (setq undo-tree-mode-lighter "")
   (setq undo-tree-visualizer-relative-timestamps t)
   (setq undo-tree-visualizer-timestamps t))
