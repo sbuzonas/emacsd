afc6c42f254f861c0582712e6c9e4705520ab9f4
Steve Buzonas <sbuzonas@carnegielearning.com>
2014-02-16 14:25:28 -0500

    added smex shortcut with C-x C-m for easier usage

--- add-ons/smex.el
+++ add-ons/smex.el
@@ -17,6 +17,8 @@
   (global-set-key (kbd "S-M-x") 'smex-major-mode-commands)
   ;; expose the old "M-x"
   (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+  ;; Use C-x C-m to do M-x per Steve Yegge's advice
+  (global-set-key (kbd "C-x C-m") 'smex)
   
   ;; Modify smex so that typing a space will insert a hyphon '-' like normal M-x
   (defadvice smex (around space-inserts-hyphen activate compile)
