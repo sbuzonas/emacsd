485ac27b20c466ef3cb87a0d6296c4e2fdc22c84
Steve Buzonas <steve@fancyguy.com>
2014-04-06 02:19:53 -0400

    initial modification for add-ons to be contained within a macro

--- add-ons.el
+++ add-ons.el
@@ -1,7 +1,29 @@
-;; Define the features group for Custom to allow addons to show their states
-(defgroup features nil
-  "Group of features allowing to toggle their configuration"
+;; Define the extensions group for Custom to allow extensions to be toggled on and off
+(defgroup addons nil
+  "Addons the current configuration is aware of and whether or not they are enabled"
   :group 'emacs)
+
+(defmacro defaddon (symbol doc &optional require &rest forms)
+  (if require
+      (progn
+	`(defcustom ,symbol
+	   nil
+	   ,doc
+           :set (lambda (symbol value)
+		  (when value
+		    `(funcall `(function ,(lambda () @forms)))))
+	   :type 'boolean
+	   :group 'addons))
+    (progn
+      `(defcustom ,symbol
+	 nil
+	 ,doc
+           :set (lambda (symbol value)
+		  (when value
+		    `(funcall `(function ,(lambda () @forms))))
+                  value)
+	 :type 'boolean
+	 :group 'addons))))
 
 (load-directory (expand-file-name "add-ons" user-emacs-directory))
 
