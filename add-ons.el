ad2cb6151794c4abd32b9a0cab2bcb0beebba564
Steve Buzonas <steve@fancyguy.com>
2014-04-06 12:12:38 -0400

    some refactoring of addon macro

--- add-ons.el
+++ add-ons.el
@@ -3,27 +3,44 @@
   "Addons the current configuration is aware of and whether or not they are enabled"
   :group 'emacs)
 
-(defmacro defaddon (symbol doc &optional require &rest forms)
-  (if require
-      (progn
-	`(defcustom ,symbol
-	   nil
-	   ,doc
-           :set (lambda (symbol value)
-		  (when value
-		    `(funcall `(function ,(lambda () @forms)))))
-	   :type 'boolean
-	   :group 'addons))
-    (progn
-      `(defcustom ,symbol
-	 nil
-	 ,doc
-           :set (lambda (symbol value)
-		  (when value
-		    `(funcall `(function ,(lambda () @forms))))
-                  value)
-	 :type 'boolean
-	 :group 'addons))))
+(defmacro defaddon (symbol require &rest forms)
+  (let ((args '(:type 'boolean :group 'addons))
+        req)
+    `,(if require
+          (progn
+            (setq req (list :require require))))
+    `(defcustom ,symbol
+       nil
+       ,(format "Functionality for %s" symbol)
+       :set (lambda (symbol value)
+              `(funcall (message "hello"))
+              ;; Return the new value
+              value)
+       ,@(append args req))))
+;;  (if require
+;;      (progn
+;;	`(defcustom ,symbol
+;;	   nil
+;;	   ,doc
+;;           :set (lambda (symbol value)
+;;		  (when value
+;;		    `(funcall `(function ,(lambda () @forms)))))
+;;	   :type 'boolean
+;;	   :group 'addons))
+;;    (progn
+;;      `(defcustom ,symbol
+;;	 nil
+;;	 ,doc
+;;           :set (lambda (symbol value)
+;;		  (when value
+;;		    `(funcall `(function ,(lambda () @forms))))
+;;                  value)
+;;	 :type 'boolean
+;;	 :group 'addons))))
+
+;;(defaddon test
+;;  'smex
+;;  (message "Hello, world!"))
 
 (load-directory (expand-file-name "add-ons" user-emacs-directory))
 
