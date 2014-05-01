2de105581a732ddc5f50eb6838e0e59bd4810b79
Steve Buzonas <steve@fancyguy.com>
2014-05-01 19:13:31 -0400

    centralize autosaves

--- conf/backups.el
+++ conf/backups.el
@@ -1,7 +1,12 @@
 ;; Write backup files to own directory
 (setq backup-directory-alist
-      `(("." .,(expand-file-name
-                (concat user-emacs-directory "backups")))))
+      `((".*" . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Write autosave files to own directory
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name
+                (concat user-emacs-directory "backups")) t)))
 
 ;; Make backups of files, even when they're in version control
 (setq vc-make-backup-files t)
