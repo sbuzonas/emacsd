027d273ad04d0d27e90835914406545432d1d7b5
Steven Buzonas <sbuzonas@poseidon.fancyguy.com>
2015-01-04 21:54:56 -0500

    update appearance

--- add-ons/appearance.el
+++ add-ons/appearance.el
@@ -6,6 +6,10 @@
     "Default theme to load at startup")
 
   (defvar fg/theme-mode-package-alist '((afternoon . afternoon-theme)
+					(alect-light . alect-themes)
+					(alect-light-alt . alect-themes)
+					(alect-dark . alect-themes)
+					(alect-dark-alt . alect-themes)
 					(ample . ample-theme)
 					(ample-zen . ample-zen-theme)
 					(anti-zenburn . anti-zenburn-theme)
@@ -22,7 +26,6 @@
 					(base16-solarized . base16-theme)
 					(base16-tomorrow . base16-theme)
 					(basic . basic-theme)
-					(birds-of-paradise . birds-of-paradise-theme)
 					(bliss . bliss-theme)
 					(boron . boron-theme)
 					(bubbleberry . bubbleberry-theme)
@@ -38,45 +41,66 @@
 					(zenburn . zenburn-theme))
     "Association list of (theme-name . theme-package) cons cells")
 
+  ;(add-to-list 'fg/theme-mode-package-alist '(birds-of-paradise . birds-of-paradise-plus-theme))
+
   (defvar fg/blacklisted-themes-list '()
     "Themes that should not be loaded for various reasons.")
-
+  
   (global-set-key (kbd "C-c n") 'fg/cycle-theme)
   (global-set-key (kbd "C-c b") 'fg/blacklist-current-theme)
-  
+
   (defun fg/available-themes ()
     (delq nil (mapcar #'(lambda (x)
 		(if (member (car x) fg/blacklisted-themes-list)
 		    nil
 		  (car x)))
 		      fg/theme-mode-package-alist)))
+  
+  (defun fg/install-all-available-themes ()
+    (let ((available-themes (fg/available-themes)))
+      (fg/require-packages
+       (delete-dups (mapcar #'cdr
+			    (mapcar #'(lambda (x)
+					(assoc x fg/theme-mode-package-alist))
+				    available-themes))))))
 
+  (fg/install-all-available-themes)
+  
   (defun fg/blacklist-current-theme ()
-    (when fg/current-theme
-      (let ((old-theme fg/current-theme))
-	(fg/cycle-theme)
-	(add-to-list 'fg/blacklisted-themes-list old-theme))))
+    (interactive)
+    (if fg/current-theme
+	(let ((old-theme fg/current-theme))
+	  (fg/cycle-theme)
+	  (add-to-list 'fg/blacklisted-themes-list old-theme))
+      (warn "Current theme cannot be blacklisted!")))
   (defalias 'blacklist-current-theme 'fg/blacklist-current-theme)
 
   (setq fg/current-theme nil)
+
+  ;; Unload all theme settings before loading a new theme
+  (defadvice load-theme (before theme-dont-propagate activate)
+    (mapcar #'disable-theme custom-enabled-themes))
   
   (defun fg/load-theme (theme)
     (let ((theme-package (cdr (assoc theme fg/theme-mode-package-alist))))
       (when theme-package
 	(fg/require-package theme-package)
-	(load-theme theme)
+	(with-temp-message (concat "Loading theme '" theme "'...")
+		      (load-theme theme))
+	(message "Loading theme '%s'...done." theme)
 	(setq fg/current-theme theme))))
 
   (defun fg/cycle-theme ()
     (interactive)
-    (let* ((current-theme-name (or fg/current-theme
-				   fg/default-theme))
-	   (current-theme (assoc current-theme-name fg/theme-mode-package-alist)))
+    (let ((current-theme (or fg/current-theme
+			     fg/default-theme))
+	  (available-themes (fg/available-themes)))
       (if current-theme
-	  (let* ((theme-pos (position current-theme fg/theme-mode-package-alist))
-		 (next-theme-pos (if (> (+ 2 theme-pos) (length fg/theme-mode-package-alist)) 0 (+ 1 theme-pos))))
-	    (message "next-theme: %s" next-theme-pos)
-	    (fg/load-theme (car (nth next-theme-pos fg/theme-mode-package-alist))))
+	  (let ((theme-count (length available-themes))
+		(next-theme-pos (+ 1 (position current-theme available-themes))))
+	    (when (>= next-theme-pos theme-count)
+	      (setq next-theme-pos 0))
+	    (fg/load-theme (nth next-theme-pos available-themes)))
 	(warn "Current theme was not found in the current theme alist."))))
   (defalias 'cycle-theme 'fg/cycle-theme)
   
