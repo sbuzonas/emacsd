dc54dacb375c69bdf9868b996969a9042ec98daf
Steven Buzonas <sbuzonas@poseidon.fancyguy.com>
2015-01-05 04:57:55 -0500

    add php configuration

---
+++ conf/php.el
@@ -0,0 +1,11 @@
+(defun load-php-settings ()
+  (setq indent-tabs-mode t)
+  (setq tab-stop-list (number-sequence (3 210 3)))
+  (setq tab-width 3)
+  (setq indent-line-function 'insert-tab))
+
+(defun php-define-tempo-templates ()
+  nil)
+
+(add-hook 'php-mode-hook 'load-php-settings)
+(add-hook 'after-php-addon-hook 'php-define-tempo-templates)
