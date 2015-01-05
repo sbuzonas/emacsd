45a7e49a30f58db4be5254391b104f2bdea064b8
Steven Buzonas <sbuzonas@poseidon.fancyguy.com>
2015-01-04 20:11:23 -0500

    add gpg checking and prevent emacs from failing to load if secrets file cannot be decrypted

--- core/fg-secrets.el
+++ core/fg-secrets.el
@@ -1,5 +1,10 @@
-(message "* --[ Loading encrypted variables ]--")
-(if (file-exists-p encrypted-variables-file)
-    (load encrypted-variables-file)
-  (message "No secrets found at path: %s" encrypted-variables-file))
+(require 'epg)
+
+(if (executable-find epg-gpg-program)
+    (progn
+      (message "* --[ Loading encrypted variables ]--")
+      (if (file-exists-p encrypted-variables-file)
+	  (load encrypted-variables-file)
+	(message "No secrets found at path: %s" encrypted-variables-file)))
+  (warn "Could not find gpg executable %s! Password file could not be loaded." epg-gpg-program))
 (provide 'fg-secrets)
