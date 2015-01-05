1a61c98c162465b18e5599888e7470d26e8d513a
Steve Buzonas <steve@fancyguy.com>
2015-01-04 19:44:24 -0500

    add personal settings to variables

--- conf/variables.el
+++ conf/variables.el
@@ -1,2 +1,8 @@
 (setq shared-dir (expand-file-name "~/Dropbox/emacs"))
 (setq encrypted-variables-file (expand-file-name "secrets.el.gpg" shared-dir))
+(setq user-full-name "Steve Buzonas")
+(if (fg/string-starts-with system-name "sbuzonas")
+    (setq user-mail-address "sbuzonas@carnegielearning.com"
+	  user-website "http://www.carnegielearning.com")
+  (setq user-mail-address "steve@fancyguy.com"
+	user-website "http://www.stevebuzonas.com"))
--- core/fg-core.el
+++ core/fg-core.el
@@ -40,4 +40,16 @@ Do it recursively if the third argument is not nil."
        ((and (eq isdir nil) (string= (substring path -3) ".el"))
 	(load (file-name-sans-extension fullpath)))))))
 
+(defun fg/string-starts-with (string prefix)
+  "Return t if STRING starts with PREFIX."
+  (and (string-match (rx-to-string `(: bos ,prefix) t)
+		     string)
+       t))
+
+(defun fg/string-ends-with (string suffix)
+  "Return t if STRING ends with SUFFIX."
+  (and (string-match (rx-to-string `(: ,suffix eos) t)
+		     string)
+       t))
+
 (provide 'fg-core)
