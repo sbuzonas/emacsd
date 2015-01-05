464332c087ad5f9eef7ac698a6ff5a006b4b9d3f
Steven Buzonas <sbuzonas@poseidon.fancyguy.com>
2015-01-05 04:57:43 -0500

    fill php addon with real settings

--- add-ons/php.el
+++ add-ons/php.el
@@ -1,4 +1,14 @@
 (defaddon php
   "Provides extensions developing with PHP."
-  (message "anothe message")
-  (message "PHP addon loaded."))
+  (fg/require-packages '(php-mode php-extras))
+
+  (defvar php-tempo-tags nil
+    "Tempo tags for PHP mode")
+
+  (defun fg/php-mode-init ()
+    (when (and buffer-file-name
+	       (fboundp 'gtags-create-or-update))
+      (gtags-mode t)
+      (gtags-create-or-update))
+    (eldoc-mode))
+  (add-hook 'php-mode-hook 'fg/php-mode-init))
