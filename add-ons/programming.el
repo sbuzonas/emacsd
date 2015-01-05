088a5eee09ea2dc1bf18e95fb6b644c778a4b301
Steven Buzonas <sbuzonas@poseidon.fancyguy.com>
2015-01-05 04:57:08 -0500

    add lambda font-lock to all languages

--- add-ons/programming.el
+++ add-ons/programming.el
@@ -5,10 +5,18 @@
     (font-lock-add-keywords
      nil '(("\\<\\(\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):\\)"
 	    1 font-lock-warning-face t))))
+
+  (defun fg/font-lock-lambda ()
+    (font-lock-add-keywords
+     nil `(("(\\(lambda\\>\\)"
+	    (0 (progn (compose-region (match-beginning 1) (match-end 1)
+				      ,(make-char 'greek-iso8859-7 107))
+		      nil))))))
     
   (defun fg/prog-mode-hook ()
     "Programming mode defaults"
     (require 'which-func)
+    (fg/font-lock-lambda)
     (which-function-mode t)
     (fg/font-lock-annotations))
 
