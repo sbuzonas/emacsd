8d926926def4b6e3a2a118bf4855a5be7dd29008
Steve Buzonas <sbuzonas@carnegielearning.com>
2015-01-05 19:25:24 -0500

    add keybindings to js2-mode

--- add-ons/javascript.el
+++ add-ons/javascript.el
@@ -1,5 +1,14 @@
 (defaddon javascript
   "Additional functionality for programming with javascript"
   (fg/require-packages '(js2-mode js-comint))
+  (setq inferior-js-program-command "rhino")
+
+  (defun fg/define-js2-mode-keys ()
+    (local-set-key [(control ?x) (control ?e)] 'js-send-last-sexp)
+    (local-set-key [(control meta ?x)] 'js-send-last-sexp-and-go)
+    (local-set-key [(control ?c) ?b] 'js-send-buffer)
+    (local-set-key [(control ?c) (control ?b)] 'js-send-buffer-and-go)
+    (local-set-key [(control ?c) ?l] 'js-load-file-and-go))
   
-  )
+  (add-hook 'js2-mode-hook 'fg/define-js2-mode-keys))
+
