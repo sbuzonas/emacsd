a7d29dc6fa3cb9e72588afd051becc124655efab
Steve Buzonas <steve@fancyguy.com>
2015-02-01 11:46:26 -0500

    add json configuration defaults

--- conf/javascript.el
+++ conf/javascript.el
@@ -1,3 +1,12 @@
+(defun fg/json-mode-defaults ()
+  (make-variable-buffer-local 'js-indent-level)
+  (setq indent-tabs-mode nil
+        tab-width 4
+        js-indent-level 4
+        standard-indent 4))
+
+(add-hook 'json-mode-hook (fg/json-mode-defaults))
+
 (setq js-indent-level 2)
 (setq js2-indent-level 2)
 (setq js2-basic-offset 2)
