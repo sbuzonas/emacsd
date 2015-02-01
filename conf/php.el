acffbee6a3cbf9f4b43e62f33f68bf076ba5b8c2
Steve Buzonas <steve@fancyguy.com>
2015-02-01 11:49:36 -0500

    update case indentation in php mode

--- conf/php.el
+++ conf/php.el
@@ -2,7 +2,8 @@
   (setq indent-tabs-mode t)
   (setq tab-stop-list (number-sequence (3 210 3)))
   (setq tab-width 4)
-  (setq indent-line-function 'insert-tab))
+  (setq indent-line-function 'insert-tab)
+  (c-set-offset 'case-label '+))
 
 (defun php-define-tempo-templates ()
   nil)
