1315b00a37e8331b8cc334fb57a87bd83f0eee63
Steve Buzonas <steve@fancyguy.com>
2015-02-01 11:48:42 -0500

    add php copyright insertion scripts

--- add-ons/php.el
+++ add-ons/php.el
@@ -4,7 +4,61 @@
 
   (defvar php-tempo-tags nil
     "Tempo tags for PHP mode")
+  
+  (eval-after-load 'autoinsert
+    '(define-auto-insert
+       '("\\.php\\'" . "PHP Application")
+       '(nil
+	 "<?php\n"
+	 "/**\n"
+	 `(when (and project-name
+		     (not (string= "" project-name)))
+	    (concat " * This file is part of " project-name ".\n *\n"))
+	 `(when (and auto-insert-copyright
+		     (not (string= "" auto-insert-copyright)))
+	    (concat " * Copyright (c) " (substring (current-time-string) -4) " " auto-insert-copyright "\n *\n"))
+	 " * For the full copyright and license information, please view the LICENSE\n"
+	 " * file that was distributed with this source code.\n"
+	 " */\n\n"
+	 `(when (not (string= "" (fg/assume-namespace-from-path)))
+	    (concat "namespace " (fg/assume-namespace-from-path) ";\n\n"))
+	 "class " (file-name-sans-extension (file-name-nondirectory (buffer-file-name))) "\n"
+	 "{\n"
+	 > _ "\n"
+	 "}\n"
+	 )))
 
+  (defun fg/assume-namespace-from-path ()
+    (let ((working-dir (file-name-directory (buffer-file-name)))
+	  (path nil)
+	  (namespace nil)
+	  (sources (if (boundp 'source-path) source-path "src/"))
+	  (tests (if (boundp 'tests-path) tests-path "tests/")))
+      (when (string= sources ".")
+	(setq sources (fg/dir-locals-dir)))
+      (when (string= tests ".")
+	(setq tests (fg/dir-locals-dir)))
+      (if (string-match sources	working-dir)
+	  (setq path sources
+		namespace (if (boundp 'source-namespace) source-namespace ""))
+	(setq path tests
+	      namespace (if (boundp 'tests-namespace) tests-namespace "")))
+      (when (string-match path working-dir)
+	(setq namespace (replace-regexp-in-string "/" "\\" (replace-regexp-in-string "\\(^/+\\|/\\'\\)" "" (concat namespace "/" (substring working-dir (match-end 0)))) t t))
+	namespace)))
+
+  (defun fg/dir-locals-dir ()
+    "Return the directory local variables directory.
+Code taken from `hack-dir-local-variables'."
+    (let ((variables-file (dir-locals-find-file (or (buffer-file-name) default-directory)))
+	  (dir-name nil))
+      (cond
+       ((stringp variables-file)
+	(setq dir-name (file-name-directory variables-file)))
+       ((consp variables-file)
+	(setq dir-name (nth 0 variables-file))))
+      dir-name))
+  
   (defun fg/php-mode-init ()
     (when (and buffer-file-name
 	       (fboundp 'gtags-create-or-update))
