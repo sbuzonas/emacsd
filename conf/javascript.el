46cf3766e81612ac10bc9068d4512d0f17138c08
Steve Buzonas <steve@fancyguy.com>
2015-02-04 08:10:28 -0500

    fix json mode hook

--- conf/javascript.el
+++ conf/javascript.el
@@ -1,11 +1,11 @@
 (defun fg/json-mode-defaults ()
   (make-variable-buffer-local 'js-indent-level)
-  (setq indent-tabs-mode nil
-        tab-width 4
-        js-indent-level 4
-        standard-indent 4))
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4)
+  (setq js-indent-level 4)
+  (setq standard-indent 4))
 
-(add-hook 'json-mode-hook (fg/json-mode-defaults))
+(add-hook 'json-mode-hook 'fg/json-mode-defaults)
 
 (setq js-indent-level 2)
 (setq js2-indent-level 2)
